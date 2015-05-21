573c1a1e55e58689c00ccc08b7e49e25563de605
Paul Cluver <paulcl@sahomeloans.com>
2015-05-21 12:48:20 +0200

    Updated stuff

--- app/app.js
+++ app/app.js
@@ -7,8 +7,7 @@
     baguaApp.constant('VERSION', '0.1');
 
     baguaApp.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
-               
-
+        
         $urlRouterProvider.otherwise('/home');
         $stateProvider
             .state('home', {
@@ -16,77 +15,88 @@
                 templateUrl: 'partials/home/home.html',
                 controller: 'homeCtrl',
                 data: {
-                    pageTitle : 'Bagua Kung Fu Durban - Home'
+                    pageTitle : 'Bagua Kung Fu Durban - Home',
+                    description: 'Bagua Zhang Kung ZhangFu Durban South Africa'
                 }
             })
             .state('history', {
                 url: '/history',
                 templateUrl: 'partials/history/history.html',
                 data: {
-                    pageTitle : 'Bagua Kung Fu Durban - History'
+                    pageTitle : 'Bagua Kung Fu Durban - History',
+                    description: 'This page provides history on the martial art of Bagua Zhang Kung Fu.'
                 }
             })
             .state('theory', {
                 url: '/theory',
                 templateUrl: 'partials/theory/theory.html',
                 data: {
-                    pageTitle : 'Bagua Kung Fu Durban - Theory'
+                    pageTitle : 'Bagua Kung Fu Durban - Theory',
+                    description: 'This page describes the theories contained within the martial art of Bagua Zhang Kung Fu.',
                 }
             })
             .state('theLion', {
                 url: '/theLion',
                 templateUrl: 'partials/animalSystems/lion/lion.html',
                 data: {
-                    pageTitle : 'Bagua Kung Fu Durban - The Lion System'
+                    pageTitle : 'Bagua Kung Fu Durban - The Lion System',
+                    description: 'This page describes the Lion system of the martial art of Bagua Zhang Kung Fu.'
                 }
             })
             .state('theSnake', {
                 url: '/theSnake',
                 templateUrl: 'partials/animalSystems/snake/snake.html',
                 data: {
-                    pageTitle : 'Bagua Kung Fu Durban - The Snake System'
+                    pageTitle : 'Bagua Kung Fu Durban - The Snake System',
+                    description: 'This page describes the Snake system of the martial art of Bagua Zhang Kung Fu.'
                 }
             })
             .state('theBear', {
                 url: '/theBear',
                 templateUrl: 'partials/animalSystems/bear/bear.html',
                 data: {
-                    pageTitle : 'Bagua Kung Fu Durban - The Bear System'
+                    pageTitle : 'Bagua Kung Fu Durban - The Bear System',
+                    description: 'This page describes the Bear system of the martial art of Bagua Zhang Kung Fu.'
                 }
             })
             .state('theDragon', {
                 url: '/theDragon',
                 templateUrl: 'partials/animalSystems/dragon/dragon.html',
                 data: {
-                    pageTitle : 'Bagua Kung Fu Durban - The Dragon System'
+                    pageTitle : 'Bagua Kung Fu Durban - The Dragon System',
+                    description: 'This page describes the Dragon system of the martial art of Bagua Zhang Kung Fu.'
                 }
             })
             .state('thePhoenix', {
                 url: '/thePhoenix',
                 templateUrl: 'partials/animalSystems/phoenix/phoenix.html',
                 data: {
-                    pageTitle : 'Bagua Kung Fu Durban - The Phoenix System'
+                    pageTitle : 'Bagua Kung Fu Durban - The Phoenix System',
+                    description: 'This page describes the Phoenix system of the martial art of Bagua Zhang Kung Fu.'
                 }
             })
             .state('theRooster', {
                 url: '/theRooster',
                 templateUrl: 'partials/animalSystems/rooster/rooster.html',
                 data: {
-                    pageTitle : 'Bagua Kung Fu Durban - The Rooster System'
+                    pageTitle : 'Bagua Kung Fu Durban - The Rooster System',
+                    description: 'This page describes the Rooster system of the martial art of Bagua Zhang Kung Fu.'
                 }
             })
             .state('theMonkey', {
                 url: '/theMonkey',
                 templateUrl: 'partials/animalSystems/monkey/monkey.html',
                 data: {
-                    pageTitle : 'Bagua Kung Fu Durban - The Monkey System'
+                    pageTitle : 'Bagua Kung Fu Durban - The Monkey System',
+                    description: 'This page describes the Monkey system of the martial art of Bagua Zhang Kung Fu.'
                 }
             })
             .state('theUnicorn', {
                 url: '/theUnicorn',
                 templateUrl: 'partials/animalSystems/unicorn/unicorn.html',
                 data: {
-                    pageTitle : 'Bagua Kung Fu Durban - The Unicorn System'
+                    pageTitle : 'Bagua Kung Fu Durban - The Unicorn System',
+                    description: 'This page describes the Unicorn system of the martial art of Bagua Zhang Kung Fu.'
                 }
             })
             .state('classes', {
@@ -94,7 +104,8 @@
                 templateUrl: 'partials/classes/classes.html',
                 controller: 'classesCtrl',
                 data: {
-                    pageTitle : 'Bagua Kung Fu Durban - Classes'
+                    pageTitle : 'Bagua Kung Fu Durban - Classes',
+                    description: 'This page provides class information on Durban Bagua Zhang Kung Fu.'
                 }
             })
             .state('contact', {
@@ -102,14 +113,16 @@
                 templateUrl: 'partials/contact/contact.html',
                 controller: 'contactCtrl',
                 data: {
-                    pageTitle : 'Bagua Kung Fu Durban - Contact'
+                    pageTitle : 'Bagua Kung Fu Durban - Contact',
+                    description: 'This page provides contact information on Durban Bagua Zhang Kung Fu.'
                 }
             })    
             .state('curriculum', {
                 url: '/curriculum',
                 templateUrl: 'partials/curriculum/curriculum.html',
                 data: {
-                    pageTitle : 'Bagua Kung Fu Durban - Curriculum'
+                    pageTitle : 'Bagua Kung Fu Durban - Curriculum',
+                    description: 'This page provides curriculum information on the martial art of Bagua Zhang Kung Fu.'
                 }
             })         
     });
@@ -117,47 +130,30 @@
     baguaApp.service('ClassesDataService', function() {
         
         this.classes = [{
-            name: 'Ashtanga',
-            description: 'Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non',
-            day: 'Monday, Tuesday',
-            time: '18:00 pm',
-            duration: 1,
-            dropInPrice: 50,
-            monthlyPrice: 200,
-            location: {
-                address: '58 Gordon Road',
+            name: 'Bagua Basics',
+            description: 'In this class we focus upon the basics of Bagua Zhang which include standing, striking, turning and changing',
+            day: 'Tuesday',
+            time: '18:15pm - 19:45pm',
+            location: 
+            {
+                address: 'Positive Health, 5 Claribel Rd',
                 suburb: 'Morningside',
                 city: 'Durban',
                 province: 'Kwa-zulu Natal'
             }
-        }, {
-            name: 'Vinyasa',
-            description: 'Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non',
-            day: 'Wednesday, Thursday',
-            time: '18:00 pm',
-            duration: 1,
-            dropInPrice: 50,
-            monthlyPrice: 200,
-            location: {
+        }, 
+        {
+            name: 'Bagua Weapons',
+            description: 'In this class we focus upon the weapon systems of Bagua Zhang which include sword, sabre, spear, staff and knives',
+            day: 'Thursday',
+            time: '17:00pm - 19:00pm',
+            location: 
+            {
                 address: '58 Gordon Road',
                 suburb: 'Morningside',
                 city: 'Durban',
                 province: 'Kwa-zulu Natal'
-            }
-        }, {
-            name: 'Hatha',
-            description: 'Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non',
-            day: 'Friday',
-            time: '18:00 pm',
-            duration: 1,
-            dropInPrice: 50,
-            monthlyPrice: 200,
-            location: {
-                address: '58 Gordon Road',
-                suburb: 'Morningside',
-                city: 'Durban',
-                province: 'Kwa-zulu Natal'
-            }
+            }        
         }];
 
     });
@@ -171,21 +167,12 @@
     });
 
     baguaApp.controller('appCtrl', function($rootScope, $scope, $location, $window) {
-        
         $scope.showContactUs = function() {
             $scope.showContactUsBoolean = false;
             if (location.hash == '#/contact') {
                 $scope.showContactUsBoolean = true;
             }
             return $scope.showContactUsBoolean;
-        };
-
-        $scope.showLionBody = function() {
-            $scope.showLionBodyBoolean = false;
-            if (location.hash == '#/contact') {
-                $scope.showLionBodyBoolean = true;
-            }
-            return $scope.showLionBodyBoolean;
         };
 
     });
@@ -226,8 +213,6 @@
     baguaApp.controller('classesCtrl', ['$scope', 'ClassesService', function($scope, ClassesService) {
        
         $scope.classes = ClassesService.getAllClasses();
-        $scope.orderByField = 'name';
-        $scope.reverseSort = false;
 
     }]);
 
@@ -268,6 +253,7 @@
                     
                     $timeout(function() {
                         $rootScope.title = (toState.data && toState.data.pageTitle) ? toState.data.pageTitle : 'Bagua Kung Fu Durban';
+                        $rootScope.description = (toState.data && toState.data.description) ? toState.data.description : 'Bagua Kung Fu in Durban, South Africa';
                     });
                 };
 
